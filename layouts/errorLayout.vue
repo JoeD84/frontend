f8557a7d55867b285a3ef47f069f2d3a6116ad50
JoeD84 <Johannes.Dielmann@gmail.com>
2024-10-05 09:31:25 +0200

    Add error.vue to replace default Error Page with Custom Error Page. Add errorLayout.vue to use a non Default Layout.

---
+++ layouts/errorLayout.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <main class="container-fluid pb-container mt-main grid gap-container grid-cols-1 ">
+      <slot />    
+  </main>
+</template>
+
+<style scoped></style>
